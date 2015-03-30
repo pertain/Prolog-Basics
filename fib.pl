8bce9cf422c7b873e705df3392bd9dacceccd69b
william <4yellowpeppers@gmail.com>
2015-03-30 09:33:02 -0400

    formatting cleanup

--- fib.pl
+++ fib.pl
@@ -15,6 +15,6 @@ fib(X,Y) :- X > 1, write('0 1 '), X > 0, fib(X-1,Y,_).
 
 fib(1,1,0).
 fib(X,Y1,Y2) :- X > 1,
-				X1 is X - 1, fib(X1,Y2,Y3),
-				Y1 is Y2 + Y3,
-				write(Y1), write(' ').
+                X1 is X - 1, fib(X1,Y2,Y3),
+                Y1 is Y2 + Y3,
+                write(Y1), write(' ').
